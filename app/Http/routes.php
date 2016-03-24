03cb0d0205b6c6a00539eb4374c55c3a4f53e2b6
DaniloTerra <danilo_terra@live.com>
2016-03-24 10:49:45 -0300

    Criação da rota para consumir serviço ViaCEP

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::group(['middleware' => ['web']], function () {
+  Route::get('/cep/{cep}', function($cep) {
+    $client = new GuzzleHttp\Client();
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+    $response = $client->request('GET', 'viacep.com.br/ws/'.$cep.'/json');
 
+    return $response->getBody();
+  });
 });
